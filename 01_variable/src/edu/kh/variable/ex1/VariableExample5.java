9cbfd2aba62eecf1cab6622121a57e1b9fa9fdff
Yong Hwan Jae <ist635@naver.com>
2022-08-05 16:29:02 +0900

    20220805 수업내용

--- 01_variable/src/edu/kh/variable/ex1/VariableExample5.java
+++ 01_variable/src/edu/kh/variable/ex1/VariableExample5.java
@@ -44,11 +44,11 @@ public class VariableExample5 {
 	    // \n: 줄바꿈(개행)을 나타내는 탈출(escape) 문자
 		
 		
-		System.out.println("줄 바꼈나요??");
-		
-		
 		System.out.println("내가 추가한 내용");
 		
 		
+		//주석
+		System.out.println("새로운 코드");
+		int aaa = 10;
 	}
 }
