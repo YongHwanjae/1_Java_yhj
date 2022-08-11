bcdadcfa2a5b42983d738d77f0a3e4f2dbfdc8ce
Yong Hwan Jae <ist635@naver.com>
2022-08-11 17:47:30 +0900

    d

--- 03_control/src/edu/kh/control/loop/Practice/LoopPractice.java
+++ 03_control/src/edu/kh/control/loop/Practice/LoopPractice.java
@@ -45,11 +45,12 @@ public class LoopPractice {
 		
 		
 		for(int i = 1 ; i<=num ; i++) {
+			sum+=i;
 			if (i==num) {
-				System.out.print(i + " = ");
+				System.out.print(i + " = " +sum);
 			}
 			else { System.out.print(i+ " + ");
-		}   sum+=i;
+		}   
 		};
 		
 		
@@ -63,17 +64,36 @@ public class LoopPractice {
 			System.out.print("두 번째 숫자 : ");
 			int num2 = sc.nextInt();
 			
-			if(num1==0 | num2== 0) { 
+			if(num1<1 || num2<1) { 
 				System.out.print("1이상의 숫자를 입력해주세요.");
 			}
-			else if(num1>=num2) {for(int i = num2; num2<=num1 ; num2++) {
-				System.out.print(num2 + " ");}}
+			else { //둘다 1 미만 아님
 				
-				 else {
-					for(int i = num1; num1<=num2 ; num1++) {
-					System.out.print(num1 + " ");
-				} 
+				if(num1>num2) {
+					
+					//두 변수의 값 교환
+					int temp = num1; //temp: 8 / num1 : 8/ num2 : 4
+					num1 = num2;    // temp : 8 / nu1 : 4 / num2  4
+					num2= temp;   //  n2 =8 n1 4 te =8
+//				int start = num1;
+//				int end = num2;
+				
+//				if(num1>num2) {
+//					start = num2;
+//					end = num1;
+				}
+				for(int i = num1; i<=num2; i++) {
+					System.out.print(i + " ");
+				}
 			}
+//			else if(num1>=num2) {for(int i = num2; i<=num1 ; i++) {
+//				System.out.print(i + " ");}}
+//				
+//				 else {
+//					for(int i = num1; i<=num2 ; i++) {
+//					System.out.print(i + " ");
+//				} 
+//			}
 			
 			
 			 }
@@ -99,12 +119,12 @@ public class LoopPractice {
 		if(num<2 || num>9) {
 			System.out.println("2~9 사이 숫자만 입력하세요.");
 		} else {
-		for(int y = num; num<=9 ; num++) {
+		for(int y = num; y<=9 ; y++) {
 			
 		
-		System.out.println("======= " + num + "단==========");
+		System.out.println("======= " + y + "단==========");
 		for(int x= 1; x<=9 ; x++ ) {
-			System.out.printf("%d * %d =  %d\n", num, x, num*x);
+			System.out.printf("%d * %d =  %2d\n", y, x, y*x);
 		}}}
 		
 		
@@ -130,36 +150,123 @@ public class LoopPractice {
 		System.out.print("정수 입력 : ");
 		int num = sc.nextInt();
 		
-		for(int row = 1; row<=num; row++) {
+		for(int row = num; row>=1; row--) {
+			for (int col = 1; col<=row; col++) {
+			System.out.print("*");} System.out.println();
+			}System.out.println();
+		}
 	
-		for(int col = num; col>=row; col--) {
-			 System.out.print("*");}
-		System.out.println();}
-	}
 	
 	
 	public void practice9() {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("정수 입력 : ");
-		int num = sc.nextInt();
-		for(int i=0; i<5; i++) {
-			for(int j=0; j<4-i; j++)
-				System.out.print(" ");			
-		 for(int j=0; j<i+1; j++) {
-			System.out.print("*");}			
-		System.out.println();		}
+		int input = sc.nextInt();
+	//for문 이용	
+//		for(int row = 1 ; row<= input; row++) {
+//			for(int i = input-1 ; i>=row; i--)
+			//input - row : 초기식 바꾸기
+//				i>= row : 조건식 바꾸기
+//				여기서는 결과 같음
+				
+//				System.out.print(" ");
+		
+		
+//			for(int col = 1; col<=row; col++) {
+//				System.out.print("*");
+		
+	
+	// if문 이용 풀이
+		for(int row = 1 ; row<= input; row++) {
+		for(int col = 1; col<=input; col++) {
+			
+			if (col<=input - row) {
+				System.out.print(" ");
+			} else {
+		System.out.print("*");
+			}
+		}System.out.println();
+	
+	
+	}
 		}
+		
+		
+		
+		
 	public void practice10() {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("정수 입력 : ");
-		int num = sc.nextInt();
+		int input = sc.nextInt();
+		
+		for(int row = 1; row<=input; row++) {
+			for (int col = 1; col<=row; col++) {
+			System.out.print("*");} System.out.println();
+		}
+		
+		for(int row = input-1; row>=1; row--) {
+			for (int col = 1; col<=row; col++) {
+			System.out.print("*");} System.out.println();
+		}
+		
+		
+		
+		
+		
+		
 		
 		
 	}
-	public void practice11() {}
-	public void practice12() {}
+	public void practice11() {
+	    Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+//	for문 이용	
+		for(int row = 1 ; row<= input; row++) {
+			for(int i = input-1 ; i>=row; i--)
+//			input - row : 초기식 바꾸기
+//				i>= row : 조건식 바꾸기
+//				여기서는 결과 같음
+//				
+				System.out.print(" ");
+//		
+//		
+		for(int col = 1; col<=2*row-1; col++) {
+				System.out.print("*");}System.out.println();}
+//		
+	
+	/* if문 이용 풀이
+		for(int row = 1 ; row<= input; row++) {
+		for(int col = 1; col<=input*2-1; col++) {
+			
+			if (col<=input - row) {
+				System.out.print(" ");
+			} else {
+		System.out.print("*");
+			}
+		}System.out.println();
+		}*/
+		
+	}
+	public void practice12() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+		
+		for(int row = 1; row<=input; row++) {
+			for(int col =1; col<=input; col++) {
+				
+				//첫번째 / 마지막 줄, 칸일 때만 *출력
+				
+				if (row==1 || row == input || col==1 || col == input) {
+				System.out.print("*");}
+			else {System.out.print(" ");
+			} }System.out.println();}}
+		
 	public void practice13() {
 		Scanner sc = new Scanner(System.in);
 		
@@ -168,10 +275,13 @@ public class LoopPractice {
 		
 		int count = 0;
 		for(int i = 1; i<=num ; i++) {
-		 if(i%2==0) {System.out.print(i + " ");}
-			else if(i%3==0) {System.out.print(i + " "); }
-	
-		}
+		 if(i%2==0 || i%3==0) {System.out.print(i + " ");}
+			
+		 //2와 3의 공배수 == 6의 배수
+		 if(i%2==0 && i%3==0 ) {
+			 count++;//count 증가 
+		 }
+		}System.out.println("\ncount : " + count);
 		}
 }
 
--- 03_control/src/edu/kh/control/loop/Practice/LoopPracticeRun.java
+++ 03_control/src/edu/kh/control/loop/Practice/LoopPracticeRun.java
@@ -8,13 +8,17 @@ public class LoopPracticeRun {
 		
 //		lp.practice1();
 //		lp.practice2();
-		//lp.practice3();
-		//lp.practice4();
+//		lp.practice3();
+//		lp.practice4();
 //		lp.practice5();
 //		lp.practice6();
 //		lp.practice7();
-		lp.practice9();
-		//lp.practice13();
+		lp.practice8();
+//		lp.practice9();
+//		lp.practice10();
+//		lp.practice11();
+//		lp.practice12();				
+//		lp.practice13();
 		
 	}
 
