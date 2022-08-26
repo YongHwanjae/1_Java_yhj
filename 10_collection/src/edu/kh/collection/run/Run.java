03a39784ff7f4d53c695b66c7ec14238df9360bc
Yong Hwan Jae <ist635@naver.com>
2022-08-26 17:40:16 +0900

    0826

--- 10_collection/src/edu/kh/collection/run/Run.java
+++ 10_collection/src/edu/kh/collection/run/Run.java
@@ -1,6 +1,7 @@
 package edu.kh.collection.run;
 
 import edu.kh.collection.model.service.ListService;
+import edu.kh.collection.model.service.MapService;
 import edu.kh.collection.model.service.SetService;
 import edu.kh.collection.view.StudentView;
 
@@ -9,7 +10,7 @@ public class Run {
 		
 		ListService service1 = new ListService();
 		
-		service1.ex1();
+//		service1.ex1();
 		
 //		new StudentView().displayMenu(); // 객체 1회성 사용
 		
@@ -18,5 +19,12 @@ public class Run {
 //		service2.ex1();
 //		service2.ex2();
 //		service2.ex3();
+		
+		MapService service3 = new MapService();
+//		service3.ex1();
+//		service3.ex2();
+		service3.ex3();
+		
+		
 	}
 }
